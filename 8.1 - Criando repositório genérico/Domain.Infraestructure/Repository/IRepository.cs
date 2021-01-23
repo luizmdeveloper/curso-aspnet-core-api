dc2d92981c62c6898a102eacea2e0511b281bd84
luiz.mario <luiz.mario@fieldcorp.com.br>
2021-01-23 11:41:58 -0300

    Criando repositório genérico

---
+++ 8.1 - Criando repositório genérico/Domain.Infraestructure/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Infraestructure.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> FindAll();
+        T FindById(int Id);
+        void Save(T entity);
+        void Update(T entity);
+        void Delte(T entity);
+    }
+}
---
+++ 8.1 - Criando repositório genérico/Domain.Infraestructure/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Domain.Infraestructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected IDbConnection _connection;
+
+        public Repository() 
+        {
+            this._connection = new MySqlConnection("Server=localhost;Port=3306;Database=finaceiro;Uid=root;Pwd=root;persistsecurityinfo=True;");
+            this._connection.Open();
+        }
+
+        public void Delte(T entity)
+        {
+            _connection.Delete(entity);
+
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return _connection.GetAll<T>();
+        }
+
+        public T FindById(int Id)
+        {
+            return _connection.Get<T>(Id);
+        }
+
+        public void Save(T entity)
+        {
+            _connection.Insert<T>(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _connection.Update<T>(entity);
+        }
+    }
+}
