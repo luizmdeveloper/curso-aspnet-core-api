2444e50251e0822cd39d77513a3d865a3177aeb4
luiz.mario <luiz.mario@fieldcorp.com.br>
2021-01-23 10:30:06 -0300

    Implementando paginação server side

---
+++ 7.5 - Implementando paginação (server-sid)/finaceiro-api/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using finaceiro_api.Entity;
+using finaceiro_api.Filter;
+using finaceiro_api.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace finaceiro_api.Controllers
+{
+    [Route("v1/categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private IDbConnection _connection;
+
+        public CategoryController() 
+        {
+            this._connection = new MySqlConnection("Server=localhost;Port=3306;Database=finaceiro;Uid=root;Pwd=root;persistsecurityinfo=True;");
+            this._connection.Open();
+        }
+
+        [HttpGet]
+        public ActionResult<ResponsePagination<Category>> Get([FromQuery] [Required] CategoryFilter filter)
+        {
+            var sql = new StringBuilder(@" SELECT * FROM categories ");
+            var parameters = new Dictionary<string, object>();
+
+            if (!string.IsNullOrEmpty(filter.Name)) 
+            {
+                sql.Append(" WHERE UPPER(name) LIKE UPPER(@name)");
+                parameters.Add("name", "%" + filter.Name + "%");
+            }
+
+            sql.Append(" LIMIT @page, @size ");
+            parameters.Add("page", filter.Page != 0 ? filter.Page * filter.Size : filter.Page);
+            parameters.Add("size", filter.Size == 0 ? 5 : filter.Size);
+
+            var categories = this._connection.Query<Category>(sql.ToString(), parameters);
+            return Ok(new ResponsePagination<Category>(10, categories));
+        }
+
+        [HttpGet("{categoryId}")]
+        public ActionResult<Category> GetById(int categoryId) 
+        {
+            var sql = @" SELECT * FROM categories 
+                         WHERE id =  @id ";
+            var category = this._connection.Query<Category>(sql, new { id = categoryId }).FirstOrDefault();
+
+            if (category != null) 
+            {
+                return Ok(category);
+            }
+
+            return NotFound();
+        }
+    }
+}
