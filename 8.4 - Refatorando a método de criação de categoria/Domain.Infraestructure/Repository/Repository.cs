cef9e227e5a58aac7747fce36f6af0c2e295d55c
luiz.mario <luiz.mario@fieldcorp.com.br>
2021-01-24 09:46:42 -0300

    Refatorando a chamada ao método de criação de categoria

---
+++ 8.4 - Refatorando a método de criação de categoria/Domain.Infraestructure/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Dapper.Contrib.Extensions;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Domain.Infraestructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly IDbConnection _connection;
+
+        public Repository(IDbConnection connection) 
+        {
+            this._connection = connection;
+            this._connection.ConnectionString = "Server=localhost;Port=3306;Database=finaceiro;Uid=root;Pwd=root;persistsecurityinfo=True;";
+            this._connection.Open();
+        }
+
+        public void Delete(T entity)
+        {
+            _connection.Delete(entity);
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return _connection.GetAll<T>();
+        }
+
+        public T FindById(int Id)
+        {
+            return _connection.Get<T>(Id);
+        }
+
+        public void Save(T entity)
+        {
+            _connection.Insert<T>(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _connection.Update<T>(entity);
+        }
+    }
+}
