09db0b1bf0209e79ec5a714c571f9485afe31505
luiz.mario <luiz.mario@fieldcorp.com.br>
2021-02-06 12:07:43 -0300

    Implementando a busca paginada

---
+++ 12.3 - Implementando a busca paginada de movimento financeiro/LuizMario.Domain.Core/Entity/FinancialMoviment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LuizMario.Domain.Core.Entity
+{
+    [Table("financialmoviments")]
+    public class FinancialMoviment
+    {
+        [Dapper.Contrib.Extensions.Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tipo operação é obrigatório.")]
+        public string Type { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DatePaid { get; set; }
+        [Required(ErrorMessage = "Categoria é obrigatória.")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Pessoa é obrigatória.")]
+        public int PersonId { get; set; }
+        public string Observation { get; set; }
+
+        [Required(ErrorMessage = "Valor é obrigatório.")]
+        public decimal Value { get; set; }
+
+        [Dapper.Contrib.Extensions.Write(false)]
+        public Category Category { get; set; }
+
+        [Dapper.Contrib.Extensions.Write(false)]
+        public Person Person { get; set; }
+    }
+}
---
+++ 12.3 - Implementando a busca paginada de movimento financeiro/LuizMario.Dto/Filter/FinancialMovimentFilterDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuizMario.Dto.Filter
+{
+    public class FinancialMovimentFilterDto : BaseFilterDto
+    {
+        public int Category { get; set; }
+        public int Person { get; set; }
+        public DateTime from { get; set; }
+        public DateTime to { get; set; }
+    }
+}
